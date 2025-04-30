c1a2cd69f4d7ca3994d8b262786f3a063411a9df
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-04-30 14:39:13 +0700

    patch: update path

--- src/common/soara-cli/template/command/__name-kebab__.js
+++ src/common/soara-cli/template/command/__name-kebab__.js
@@ -1,5 +1,4 @@
-const path = require('path');
-const { registerCommand } = require(path.join(process.cwd(), 'src/templates', 'soara-registry'));
+const { registerCommand } = require('@soara/cli/src/templates/soara-registry');
 
 registerCommand('__name__', 'Create __name__', async (name) => {
   console.log('Hello __name__');
