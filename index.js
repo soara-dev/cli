611c4c48d534c8ea8758b6a0e69e65ddfe1f4a4e
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-02 15:40:49 +0700

    update index

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const templatePath = path.join(__dirname, 'src/templates', 'soara.js');
 const fileContent = fs.readFileSync(templatePath, 'utf-8');
