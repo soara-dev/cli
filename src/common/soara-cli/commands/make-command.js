38608be8b267fc6f7c9f393333838cd0508a16f6
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-02 09:56:12 +0700

    patch: move utils

--- src/common/soara-cli/commands/make-command.js
+++ src/common/soara-cli/commands/make-command.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { registerCommand } = require(path.join(__dirname, '../../../templates', 'soara-registry'));
-const { copyTemplate } = require(path.join(__dirname, '../../utils'));
+const { copyTemplate } = require(path.join(__dirname, '../../../utils'));
 
 registerCommand('make:command <name>', 'Create new Command CLI', async (name) => {
   const templatePath = path.join(__dirname, '../template/commands');
